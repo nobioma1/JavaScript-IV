329b5d9d2c98ee888b2a6c128804252d8b36f3a9
Noble Obioma <nobioma1@gmail.com>
2019-05-09 13:02:44 +0100

    lambda-classes.js -- Person Class - create person class - create constructor taking object as a parameter, having properties name, age, gender, location - add speak method on person class - instanstiate Person class using two objects jane and johnny - invoke jane and johnny speak method

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,31 @@
 // CODE here for your Lambda Classes
+class Person {
+  constructor(attributes) {
+    this._name = attributes.name;
+    this._age = attributes.age;
+    this._gender = attributes.gender;
+    this._location = attributes.location;
+  }
+
+  speak() {
+    return `Hello! My name is ${this._name}, I am from ${this._location}`;
+  }
+}
+
+const jane = new Person({
+  name: 'Jane',
+  location: 'Phillipines',
+  age: 25,
+  gender: 'Female'
+});
+
+const johnny = new Person({
+  name: 'Johnny',
+  location: 'Jamaica',
+  age: 18,
+  gender: 'male'
+});
+
+console.log(jane.speak());
+console.log(johnny.speak());
+
