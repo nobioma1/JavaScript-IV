838deaab74ba421a8018668f9e23c7da131bce87
Noble Obioma <nobioma1@gmail.com>
2019-05-09 13:39:51 +0100

    lambda-classes.js -- Student Class - create student class - create constructor taking object as a parameter,     having properties inherited from Person class     and its own unique properties previousBackground, className,     favSubjects. - add listsSubjects method that logs all students favorite subjects - add PRAssignment method that takes a parameter subject     and return a string. - add sprintChallenge method that takes a parameter subject     and return a string. - instanstiate student class using two objects Precious and manny - invoke Precious and manny speak, listSubjects, sprintChallenge methods

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,4 +1,7 @@
 // CODE here for your Lambda Classes
+/**
+ * @class Person
+ */
 class Person {
   constructor(attributes) {
     this._name = attributes.name;
@@ -29,6 +32,10 @@ const johnny = new Person({
 console.log(jane.speak());
 console.log(johnny.speak());
 
+/**
+ * @class Instructor
+ * @extends {Person}
+ */
 class Instructor extends Person {
   constructor(attributes) {
     super(attributes);
@@ -72,3 +79,55 @@ console.log(mark.grade({name: 'Jane'}, 'Python'));
 console.log(mary.speak());
 console.log(mary.demo('Python'));
 console.log(mary.grade({name: 'John'}, 'HTML'));
+
+/**
+ * @class Student
+ * @extends {Person}
+ */
+class Student extends Person {
+  constructor(attributes) {
+    super(attributes);
+    this._previousBackground = attributes.previousBackground;
+    this._className = attributes.className;
+    this._favSubjects = attributes.favSubjects;
+  }
+
+  listsSubjects() {
+    this._favSubjects.forEach(subject => console.log(subject));
+  }
+
+  PRAssignment(subject) {
+    return `${this._name} has submitted a PR for ${subject}`
+  }
+
+  sprintChallenge(subject) {
+    return `${this._name} has begun sprint challenge on ${subject}`
+  }
+}
+
+const precious = new Student({
+  name: 'Precious',
+  location: 'Nigeria',
+  age: 20,
+  gender: 'Female',
+  previousBackground: 'Fine Art',
+  className: 'WebEU5',
+  favSubjects: ['Art', 'CSS', 'LESS', 'Web Design']
+});
+
+const manny = new Student({
+  name: 'Manny',
+  location: 'Ivory Coast',
+  age: 21,
+  gender: 'Male',
+  previousBackground: 'Computer Science',
+  className: 'WebUS1',
+  favSubjects: ['French', 'Java']
+});
+
+console.log(precious.speak());
+precious.listsSubjects();
+console.log(precious.sprintChallenge('LESS'));
+console.log(manny.speak());
+manny.listsSubjects();
+console.log(manny.sprintChallenge('Java'));
